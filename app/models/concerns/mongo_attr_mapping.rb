f26f62014a3e47b5a9b637ed84ca44850abea8fa
Thomas Schmidt <tschmidt@suse.de>
2022-01-02 02:17:59 +0100

    don't set overwrite field with the same value as the overwritten field

--- app/models/concerns/mongo_attr_mapping.rb
+++ app/models/concerns/mongo_attr_mapping.rb
@@ -1,17 +1,26 @@
 require 'active_support/concern'
 
+# We need `read_attribute` here to make eg. 'ldap.title' work
+# rubocop:disable Rails/ReadWriteAttribute
+
 module MongoAttrMapping
   extend ActiveSupport::Concern
 
   class_methods do
     def attribute_mapping(name, target, overwriteable: false)
       define_method name do
-        # We need `read_attribute` here to make eg. 'ldap.title' work
-        # rubocop:disable Rails/ReadWriteAttribute
         self[name] || read_attribute(target)
-        # rubocop:enable Rails/ReadWriteAttribute
       end
-      field(name, type: String) if overwriteable
+      return unless overwriteable
+
+      define_method "#{name}=" do |value|
+        # reset value if it's the same as the shadowed field
+        value = nil if value == read_attribute(target)
+        self[name] = value
+      end
+      field(name, type: String)
     end
   end
 end
+
+# rubocop:enable Rails/ReadWriteAttribute
