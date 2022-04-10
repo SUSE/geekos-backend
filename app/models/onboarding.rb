760871ae9db3ec1edc9cf60c990e48414589e890
Thomas Schmidt <tschmidt@suse.de>
2022-04-10 22:29:19 +0200

    bump coverage to 100

--- app/models/onboarding.rb
+++ app/models/onboarding.rb
@@ -1,9 +1,8 @@
 class Onboarding
-  LISTS = (ENV['geekos_trello_lists'] || '').split(',')
-
   def self.chapters
     @chapters ||= {}
-    LISTS.each do |listid|
+    lists = (ENV['geekos_trello_lists'] || '').split(',')
+    lists.each do |listid|
       list = Trello::List.find(listid)
       @chapters[list.name] = list.cards
     end
